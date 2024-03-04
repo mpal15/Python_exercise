9e854518f7e9375ad84c62489647b17c39d80bfa
Mohit pal <mohitpal9513@gmail.com>
2024-03-04 10:09:14 +0530

    exercise loop in python

---
+++ 03_loops/04_Solution.py
@@ -0,0 +1,7 @@
+input_str = "Pyhton"
+reversed_str = ""
+
+for chai in input_str:
+    reversed_str =chai + reversed_str
+
+print(reversed_str)
---
+++ 03_loops/06_Solution.py
@@ -0,0 +1,8 @@
+number = 3
+fact =1
+
+while number > 0:
+    fact= fact*number
+    number= number-1
+
+print(fact)
